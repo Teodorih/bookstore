95526928623b5d6db4ff28118907e4d6fa3c167a
Victor Ivanov <Teodorih@hotmail.com>
2021-04-26 13:04:33 +0300

    add new tests + fix old

--- catalog/views.py
+++ catalog/views.py
@@ -105,7 +105,7 @@ def renew_book_librarian(request, pk):
             book_inst.save()
 
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed') )
+            return HttpResponseRedirect(reverse('on-loan-books') )
 
     # If this is a GET (or any other method) create the default form.
     else:
