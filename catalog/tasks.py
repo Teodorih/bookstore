cedc4160b2a66a94796648ff6eab1c96ac7c80bf
Teodorih <Teodorih@hotmail.com>
2021-07-28 21:54:19 +0300

    fix celery, run with celery -A catalog.tasks worker -l info

--- catalog/tasks.py
+++ catalog/tasks.py
@@ -1,7 +1,12 @@
 from celery import Celery
 
-app = Celery('tasks', broker='redis://localhost:6379')
+app = Celery('catalog', broker='redis://localhost:6379',  include=['catalog.tasks'])
+i = 1 +2
 
+app.conf.update(Celery_TAST_RESULT_EXPIRES=3600,)
+
+if __name__ == '__main__':
+    app.start()
 
 @app.task
 def add(x, y):
